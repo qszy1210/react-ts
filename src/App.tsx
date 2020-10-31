365c54ebf77a3b7f9be5b4afaccab7cd2bd5d4e2
张青松 <zhangqsb@chanjet.com>
2020-10-31 23:54:45 +0800

    add history dir

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import WhyReact from './components/WhyReact';
-
+import { MyComponent } from './Amis';
+import '../node_modules/amis/lib/themes/default.css';
 const App: React.FC = () => {
   return (
     <div className="App">
-      <WhyReact></WhyReact>
+      <MyComponent></MyComponent>
     </div>
   );
 }
---
+++ src/components/TestJs/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import "./index.css";
+
+class TestJs extends React.Component {
+  constructor(props, context) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <Temp>
+        <div />
+      </Temp>
+    );
+  }
+}
+
+export default TestJs;
